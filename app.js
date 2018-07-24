560150e47aca451c8ab3fe868ff7b122718a17c3
Sam <Sstauffer.13@gmail.com>
2018-07-23 19:06:06 -0500

    merp

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('flamethrower-link').onclick = (e) => {
+    let player = document.getElementById('flamethrower-video');
+    if (player.requestFullscreen) {
+        player.requestFullscreen();
+    } else if (player.msRequestFullscreen) {
+        player.msRequestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+        player.mozRequestFullScreen();
+    } else if (player.webkitRequestFullscreen) {
+        player.webkitRequestFullscreen();
+    }
+}
